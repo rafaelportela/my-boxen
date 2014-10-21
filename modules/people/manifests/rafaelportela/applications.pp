ab68265c88c6cc1f78c6f4189c0a5595328b3ea5
Rafael Portela <rafaelportela90@gmail.com>
2014-10-21 18:04:43 +0200

    Add Intellij.

--- modules/people/manifests/rafaelportela/applications.pp
+++ modules/people/manifests/rafaelportela/applications.pp
@@ -3,4 +3,9 @@ class people::rafaelportela::applications {
   include chrome
   include alfred
   include evernote
+
+  class { 'intellij':
+    edition => 'ultimate',
+    version => '13.1.5',
+  }
 }
